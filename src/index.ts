aecac5ce799490a996ab1a3f09d5073f055d2b0a
mariakanderson <mariakanderson@yahoo.com>
2025-03-30 21:32:41 +0000

    Optimize auth - fixing typo

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { createServer } from "./server.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
 async function main() {
-  const transport = new StdioServerTransport();
-  const server = createServer();
+  let transport = new StdioServerTransport();
+  let server = createServer();
   await server.connect(transport);
 }
 
